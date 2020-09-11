b9deafbd048ef7efbe00999ae98aabb0e92acaa2
mohamedhan <mohamedhanafihassanet@gmail.com>
2020-09-11 10:46:41 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express=require("express")
+const app=express()
+const connectDB=require("./config/connectDB")
+// middleware
+app.use(express.json())
+// connect database
+connectDB()
+
+// routes
+app.use("/contacts",require("./routes/contact"))
+
+// ruun server
+const port =process.env.PORT||5000
+app.listen(port,err=>
+    err? console.log("erreur"):console.log(`Server is running on port ${port}`)
+    )
